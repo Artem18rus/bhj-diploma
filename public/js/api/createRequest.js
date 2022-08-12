3002e223569eb023edb8e4a07dbd734a7558865b
Artem na rab <urart88@mail.ru>
2022-08-12 14:42:51 +0400

    Диплом JS в браузере v1

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,37 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const {url, data, method, callback} = options;
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
 
-};
+   if(method === 'GET') {
+    try {
+      const paramsGet = url + '?mail=' + data.mail + '&password=' + data.password;
+      xhr.open(method, paramsGet);
+      xhr.send();
+    }
+    catch (err) {
+      callback(err);
+    }
+  } else {
+    try {
+      const formData = new FormData();
+      formData.append('mail', data.mail);
+      formData.append('password', data.password);
+      xhr.open(method, url);
+      xhr.send(formData);
+    }
+    catch (err) {
+      callback(err);
+    }
+  }
+
+  xhr.onload = function() {
+    if(xhr.status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(err);
+    }
+  }
+}
